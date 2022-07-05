901940c02b5a7c43386858b5da6b65c06061db3e
vigourlee88 <vigourlee137@gmail.com>
2022-07-05 14:04:44 +0900

    2022/07/05/

---
+++ practice/src/main/java/Store03.java
@@ -0,0 +1,18 @@
+public class Store03 {
+	//instance fields
+	String productType;
+	
+	//constructor method
+	public Store03(String product) {
+		productType = product;
+	}
+	
+	//main method Assigning Values to Instance Fields
+	public static void main(String[] args) {
+		Store03 lemonadeStand = new Store03("lemonade");
+		System.out.println(lemonadeStand.productType);
+		
+		
+		
+	}
+}
